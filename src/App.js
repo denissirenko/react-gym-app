418b090ce51eef23acdaf46ff4932e0866510c5a
Denis Sirenko <sirenko.denis86@gmail.com>
2022-07-22 12:53:50 +0300

    add file & folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/system';
+
+import { ExerciseDetail } from './pages/ExerciseDetail';
+import { Home } from './pages/Home';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
 import './App.css';
 
 function App() {
-  return <div className="App">Hello!!!</div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Navbar = () => {
+  console.log('Navbar');
+  return <div>Navbar</div>;
+};
