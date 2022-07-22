6285414780969070fb644257ccc58e852a6d1b07
Denis Sirenko <sirenko.denis86@gmail.com>
2022-07-22 14:49:50 +0300

    add hero banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Box } from '@mui/material';
+
+import { Exercises } from '../components/Exercises';
+import { SearchExercises } from '../components/SearchExercises';
+import { HeroBanner } from '../components/HeroBanner';
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 };
